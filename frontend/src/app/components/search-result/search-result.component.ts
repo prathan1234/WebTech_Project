02f0c2dcf0abb6569fc5544a9fcd5dd41c43c073
prathan1234 <prathan.psong@gmail.com>
2017-12-15 22:04:30 +0700

    Add search all

--- frontend/src/app/components/search-result/search-result.component.ts
+++ frontend/src/app/components/search-result/search-result.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EventService } from '../../services/event.service';
+
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
   styleUrls: ['./search-result.component.css']
 })
 export class SearchResultComponent implements OnInit {
+  private eventList: Event[];
+  private result_text: string;
 
-  constructor() { }
+  constructor(private eventService: EventService, private router: Router) { }
 
   ngOnInit() {
+    this.getAllEvent();
   }
 
+  getAllEvent() {
+    this.eventService.getAllEvent().subscribe((response) => {
+      this.eventList = response;
+      if (response == null && this.eventList.length == 0) {
+        this.result_text = "Not found event!";
+      }
+    })
+  }
+
+  getOneEvent(id) {
+    if (id != null && id != "") {
+      this.eventService.setEventId(id);
+      this.router.navigate(['/event/' + id]);
+    }
+  }
+}
+
+interface Event {
+  event_name: string;
+  author: string;
+  location: string;
+  catagory: string;
+  content: string;
+  start_time: Date;
+  end_time: Date;
 }
